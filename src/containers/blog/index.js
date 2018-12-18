2d6216a233a08a81ca233327e5acde18dbdef28f
Chris Fenos <chris.m.fenos@gmail.com>
2018-12-17 22:01:09 -0500

    inline style removed

--- src/containers/blog/index.js
+++ src/containers/blog/index.js
@@ -7,65 +7,75 @@ import articles from './assets/articles';
 
 const { Content } = Layout;
 
-const listData = [];
-for (let i = 0; i < 10; i++) {
-  listData.push({
-    href: 'https://github.com/goerli/testnet/edit/master/README.md',
-    title: `Example Blog Post ${i}`,
-    avatar: require('../../assets/images/goerli.png'),
-    description: 'Awesome content coming....',
-    content:`There are many Ethereum testnets available for experimenting with smart contracts and deploying
-    decentralised applications before going live on the main Ethereum network. However, there is no testnet 
-    available that is both widely usable across all client implementations, and robust enough to guarantee 
-    consistent availability and high reliability.\n`,
-  });
-}
-
 const BlogPage = () => (
-  <Layout style={layoutStyle}>
-  <NavigationHeader />
-  <Layout>
-    <SiderMenu />
-    <Layout style={{ padding: '0 24px 24px', height: '100%' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
+  <Layout style={layoutContainerStyle}>
+    <NavigationHeader />
+    <Layout>
+      <SiderMenu />
+      <Layout style={layoutStyle}>
+        <Breadcrumb style={breadcrumbStyle}>
           <Breadcrumb.Item>Network</Breadcrumb.Item>
           <Breadcrumb.Item>Blog</Breadcrumb.Item>
-      </Breadcrumb>
-      <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-        <h3 style={{ margin: '16px 0' }}>Goerli Articles</h3>
-        <List
-          itemLayout="vertical"
-          size="large"
-          pagination={{
-            onChange: (page) => {
-              console.log(page);
-            },
-            pageSize: 10,
-          }}
-          dataSource={articles}
-          renderItem={item => (
-            <List.Item
-              key={item.title}           
-            >
-              <List.Item.Meta
-                avatar={<Avatar src={item.avatar} />}
-                title={<a href={item.href} target="_blank" rel="noopener noreferrer">{item.title}</a>}
-                description={item.meta}
-              />
-              {item.content}
-              <p style={{paddingTop: '2.5%'}}> <a href={item.href} target="_blank" rel="noopener noreferrer">Read Article </a> </p>
-            </List.Item>
-          )}
-        />
-      </Content>
+        </Breadcrumb>
+        <Content style={contentStyle}>
+          <h3 style={headerStyle}>Goerli Articles</h3>
+          <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+              onChange: (page) => {
+                console.log(page);
+              },
+              pageSize: 10,
+            }}
+            dataSource={articles}
+            renderItem={item => (
+              <List.Item
+                key={item.title}           
+              >
+                <List.Item.Meta
+                  avatar={<Avatar src={item.avatar} />}
+                  title={<a href={item.href} target="_blank" rel="noopener noreferrer">{item.title}</a>}
+                  description={item.meta}
+                />
+                {item.content}
+                <p style={linkStyle}> <a href={item.href} target="_blank" rel="noopener noreferrer">Read Article </a> </p>
+              </List.Item>
+            )}
+          />
+        </Content>
+      </Layout>
     </Layout>
   </Layout>
-</Layout>
 );
 
-const layoutStyle = {
+const layoutContainerStyle = {
   flex: 1, 
   height: '100vh'
+};
+
+const layoutStyle = {
+  padding: '0 24px 24px',
+  height: '100%',
+};
+
+const breadcrumbStyle = {
+  margin: '16px 0',
+};
+
+const contentStyle = {
+  background: '#fff',
+  padding: 24,
+  margin: 0,
+  minHeight: 280, 
+};
+
+const headerStyle = {
+  margin: '16px 0',
+};
+
+const linkStyle = {
+  paddingTop: '2.5%',
 };
 
 export default BlogPage;
